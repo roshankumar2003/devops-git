22326d862d2ac092d9ed4c99db573912ed39516a
roshankumar2003 <94346289+roshankumar2003@users.noreply.github.com>
2024-03-01 10:19:30 +0530

    Create jugsmugsbugs.py
    created jugs mugs bugs python program

---
+++ jugsmugsbugs.py
@@ -0,0 +1,30 @@
+number = int(input("Enter the number :"))
+res=""
+if  number %3 ==0:
+  res+="jugs"
+if  number %5 ==0:
+  res+="mugs"
+if number %7 ==0:
+  res+="pugs"
+
+
+print(res)
+
+2.
+
+number = int(input("Enter the number :"))
+string = str(number)
+result =[]
+res=""
+if  number %3 ==0:
+  result.append("jugs")
+if  number %5 ==0:
+  result.append("mugs")
+if number %7 ==0:
+  result.append("pugs")
+
+for i in range(len(result)):
+  res += result.pop()
+print(res)
+
+
